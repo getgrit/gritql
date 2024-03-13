cbf1abf9c4074fdc0c0151d2b53589b2b0f0f0e7
ilevyor <81260226+ilevyor@users.noreply.github.com>
2024-03-13 10:56:03 -0400

    fix: wasm feature doesn't break build (#15)

--- crates/core/src/pattern/function_definition.rs
+++ crates/core/src/pattern/function_definition.rs
@@ -240,7 +240,10 @@ impl FunctionDefinition for ForeignFunctionDefinition {
         let resolved_str: Vec<&str> = cow_resolved.iter().map(Cow::as_ref).collect();
 
         // START Simple externalized version
-        #[cfg(feature = "external_functions_ffi")]
+        #[cfg(all(
+            feature = "external_functions_ffi",
+            target_arch = "wasm32"
+        ))]
         let result = (context.runtime.exec_external)(&self.code, param_names, &resolved_str)?;
 
         // END Simple externalized version
