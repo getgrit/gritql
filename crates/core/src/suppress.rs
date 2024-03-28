4a4fea0e056a4acd730350ece7f4863c20668a88
ilevyor <itamar.levyor+github@gmail.com>
2024-03-28 13:26:07 -0400

    char

--- crates/core/src/suppress.rs
+++ crates/core/src/suppress.rs
@@ -115,7 +115,7 @@ fn comment_occupies_entire_line(text: &str, range: &Range, src: &str) -> bool {
     src.lines()
         .skip(range.start_point().row() as usize)
         .take((range.end_point().row() - range.start_point().row() + 1) as usize)
-        .zip_longest(text.split("\n"))
+        .zip_longest(text.split('\n'))
         .all(|zipped| {
             if let EitherOrBoth::Both(src_line, text_line) = zipped {
                 src_line.trim() == text_line.trim()
