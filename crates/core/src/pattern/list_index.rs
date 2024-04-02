82f53cb6151e4dc832253eb443fc72902b32ace4
ilevyor <itamar.levyor+github@gmail.com>
2024-04-02 14:01:25 -0400

    revert: reset to binding is suppressed

--- crates/core/src/pattern/list_index.rs
+++ crates/core/src/pattern/list_index.rs
@@ -12,8 +12,7 @@ use crate::binding::{Binding, Constant};
 use crate::context::Context;
 use crate::resolve_opt;
 use anyhow::{anyhow, bail, Result};
-use im::vector;
-use marzano_util::{analysis_logs::AnalysisLogs, tree_sitter_util::named_children_by_field_id};
+use marzano_util::analysis_logs::AnalysisLogs;
 use std::collections::BTreeMap;
 use tree_sitter::Node;
 
@@ -129,21 +128,16 @@ impl ListIndex {
                     Ok(l.get(index).map(PatternOrResolved::Pattern))
                 }
                 Some(PatternOrResolved::Resolved(ResolvedPattern::Binding(b))) => {
-                    let binding = b
+                    let mut list_items = b
                         .last()
+                        .and_then(Binding::list_items)
                         .ok_or_else(|| anyhow!("left side of a listIndex must be a list"))?;
-                    if let Binding::List(src, node, field) = binding {
-                        let mut cursor = node.walk();
-                        let len = named_children_by_field_id(node, &mut cursor, *field).count();
-                        let mut list = named_children_by_field_id(node, &mut cursor, *field);
-                        let index = resolve_opt!(to_unsigned(index, len));
-                        return Ok(list.nth(index).map(|n| {
-                            PatternOrResolved::ResolvedBinding(ResolvedPattern::Binding(vector![
-                                Binding::Node(src, n)
-                            ]))
-                        }));
-                    }
-                    bail!("left side of a listIndex must be a list")
+
+                    let len = list_items.clone().count();
+                    let index = resolve_opt!(to_unsigned(index, len));
+                    return Ok(list_items.nth(index).map(|n| {
+                        PatternOrResolved::ResolvedBinding(ResolvedPattern::from_node(n))
+                    }));
                 }
                 Some(PatternOrResolved::Resolved(ResolvedPattern::List(l))) => {
                     let index = resolve_opt!(to_unsigned(index, l.len()));
@@ -167,19 +161,16 @@ impl ListIndex {
                     Ok(l.get(index).map(PatternOrResolvedMut::Pattern))
                 }
                 Some(PatternOrResolvedMut::Resolved(ResolvedPattern::Binding(b))) => {
-                    let binding = b
+                    let mut list_items = b
                         .last()
+                        .and_then(Binding::list_items)
                         .ok_or_else(|| anyhow!("left side of a listIndex must be a list"))?;
-                    if let Binding::List(_src, node, field) = binding {
-                        let mut cursor = node.walk();
-                        let len = named_children_by_field_id(node, &mut cursor, *field).count();
-                        let mut list = named_children_by_field_id(node, &mut cursor, *field);
-                        let index = resolve_opt!(to_unsigned(index, len));
-                        return Ok(list
-                            .nth(index)
-                            .map(|_n| PatternOrResolvedMut::_ResolvedBinding));
-                    }
-                    bail!("left side of a listIndex must be a list")
+
+                    let len = list_items.clone().count();
+                    let index = resolve_opt!(to_unsigned(index, len));
+                    Ok(list_items
+                        .nth(index)
+                        .map(|_| PatternOrResolvedMut::_ResolvedBinding))
                 }
                 Some(PatternOrResolvedMut::Resolved(ResolvedPattern::List(l))) => {
                     let index = resolve_opt!(to_unsigned(index, l.len()));
