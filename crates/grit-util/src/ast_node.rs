4d32c1a0d815b142624e45064b837f93412c634a
Arend van Beelen jr <arendjr@gmail.com>
2024-04-04 03:31:22 +0200

    refactor: move node traversal to `AstNode` trait (#148)

--- crates/grit-util/src/ast_node.rs
+++ crates/grit-util/src/ast_node.rs
@@ -2,11 +2,18 @@
 ///
 /// This trait should be free from dependencies on TreeSitter.
 pub trait AstNode: Sized {
+    /// Returns an iterator over the node's ancestors, starting with the node
+    /// itself and moving up to the root.
+    fn ancestors(&self) -> impl Iterator<Item = Self>;
+
+    /// Returns an iterator over the node's children.
+    fn children(&self) -> impl Iterator<Item = Self>;
+
     /// Returns the next node, ignoring trivia such as whitespace.
-    fn next_non_trivia_node(&self) -> Option<Self>;
+    fn next_named_sibling(&self) -> Option<Self>;
 
     /// Returns the previous node, ignoring trivia such as whitespace.
-    fn previous_non_trivia_node(&self) -> Option<Self>;
+    fn previous_named_sibling(&self) -> Option<Self>;
 
     /// Returns the text representation of the node.
     fn text(&self) -> &str;
