3b643d20a590a80b99fa779482e854a0c59635ff
Morgante Pell <morgantep@google.com>
2024-04-05 13:54:59 -0700

    match it

--- crates/core/src/test.rs
+++ crates/core/src/test.rs
@@ -8551,6 +8551,57 @@ fn markdown_heading_rewrite() {
 }
 
 #[test]
+fn markdown_sections() {
+    run_test_expected({
+        TestArgExpected {
+            pattern: "
+                language markdown(block)
+
+                section($heading, $content) where {
+                    $heading <: atx_heading(level=atx_h2_marker()),
+                    $content <: not includes \"skip\"
+                } => raw`---
+                SECTION 2 HEADER: $heading`
+                "
+            .to_owned(),
+            source: r#"
+                |# File with two secionds
+                |Some content
+                |## level 2
+                |More content
+                |## level 2 (do not skip)
+                |Even more content
+                |### level 3
+                |Even more content
+                |#### level 4
+                |Even more content
+                |## Now we go back to section two
+                |Content only here
+                |## Skip this one...
+                |skip me
+                |"#
+            .trim_margin()
+            .unwrap(),
+            expected: r#"
+                |# File with two secionds
+                |Some content
+                |---
+                |                SECTION 2 HEADER: ## level 2
+                |---
+                |                SECTION 2 HEADER: ## level 2 (do not skip)
+                |---
+                |                SECTION 2 HEADER: ## Now we go back to section two
+                |## Skip this one...
+                |skip me
+                |"#
+            .trim_margin()
+            .unwrap(),
+        }
+    })
+    .unwrap();
+}
+
+#[test]
 fn parses_java_constructor() {
     run_test_expected({
         TestArgExpected {
