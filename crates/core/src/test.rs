de77e58216821cc274e27ec68805c6f3586d078b
Morgante Pell <morgantep@google.com>
2024-04-08 23:04:28 -0700

    [skip ci] yolo

--- crates/core/src/test.rs
+++ crates/core/src/test.rs
@@ -11801,7 +11801,83 @@ fn yaml_string() {
 }
 
 #[test]
-fn yaml_list_indentation() {
+fn yaml_list_keep_indentation() {
+    run_test_expected({
+        TestArgExpected {
+            pattern: r"
+                |engine marzano(0.1)
+                |language yaml
+                |
+                |`- $task` where {
+                |    $task <: block_mapping($items),
+                |    $subtasks = [],
+                |    $items <: some block_mapping_pair(key=contains `across`, $value),
+                |    $value <: contains `values: $values`,
+                |    $values <: some bubble($items, $subtasks) `- $val` where {
+                |        $new_task = [],
+                |        $items <: some bubble($new_task) $_ where {
+                |                $new_task += `foo: other`,
+                |            },
+                |        $bar = join(list=$new_task, separator=`\n  `),
+                |        $subtasks += `- $bar`
+                |    },
+                |    $foo = join(list=$subtasks, separator=`\n\n`),
+                |    $task => `in_parallel:
+                |$foo`
+                |}
+                |
+                |"
+            .trim_margin()
+            .unwrap(),
+            source: r#"
+                |  - across:
+                |    - var: name
+                |      values:
+                |      - file1
+                |      - file2
+                |      - file3
+                |    task: create-file
+                |    config:
+                |      platform: linux
+                |      image_resource:
+                |        type: registry-image
+                |        source: {repository: busybox}
+                |      run:
+                |        path: touch
+                |        args:
+                |        - manifests/((.:name))
+                |      outputs:
+                |      - name: manifests
+                |    file: input.yaml
+                |"#
+            .trim_margin()
+            .unwrap(),
+            expected: r#"
+                |  - in_parallel:
+                |    - foo: other
+                |      foo: other
+                |      foo: other
+                |      foo: other
+                |
+                |    - foo: other
+                |      foo: other
+                |      foo: other
+                |      foo: other
+                |
+                |    - foo: other
+                |      foo: other
+                |      foo: other
+                |      foo: other
+                |"#
+            .trim_margin()
+            .unwrap(),
+        }
+    })
+    .unwrap();
+}
+
+#[test]
+fn yaml_list_add_indentation() {
     run_test_expected({
         TestArgExpected {
             pattern: r"
