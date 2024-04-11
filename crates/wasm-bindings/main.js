cade44486ca5f8b896c6f18ca8d0dbdc0fbb3c86
ilevyor <81260226+ilevyor@users.noreply.github.com>
2024-04-11 16:43:54 -0400

    chore: setup initial wasm site. (#205)

---
+++ crates/wasm-bindings/main.js
@@ -0,0 +1,32 @@
+import init, { matchPattern } from './pkg/marzano_wasm_bindings.js';
+import TreeSitter from 'web-tree-sitter';
+import { helloWorld } from './pkg/marzano_wasm_bindings.js';
+
+async function match(pattern, content) {
+    return await matchPattern(
+        pattern,
+        ["playground_pattern"],
+        [content],
+        [],
+        [],
+        '',
+        '',
+    )
+}
+
+export async function myMatchPattern() {
+    await init();
+    let textArea1 = document.getElementById('textArea1').value;
+    let textArea2 = document.getElementById('textArea2').value;
+    let pattern = textArea1;
+    let content = textArea2;
+    document.getElementById('resultTextArea').value = JSON.stringify(
+        // await match(pattern, content),
+        await helloWorld(),
+    );
+}
+
+document.addEventListener('DOMContentLoaded', async (event) => {
+    document.getElementById('gritButton').addEventListener('click', myMatchPattern);
+    await init();
+});
