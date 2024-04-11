d336e05730b743bd4b0afb5b2c7f09da32c93b78
ilevyor <itamar.levyor+github@gmail.com>
2024-04-11 15:47:19 -0400

    remove match_pattern signature reference

--- crates/wasm-bindings/main.js
+++ crates/wasm-bindings/main.js
@@ -2,22 +2,6 @@ import init, { matchPattern } from './pkg/marzano_wasm_bindings.js';
 import TreeSitter from 'web-tree-sitter';
 import { helloWorld } from './pkg/marzano_wasm_bindings.js';
 
-// pub async fn matchPattern(
-//     pattern: String,
-//     // The paths of the files to match against.
-//     paths: Vec < String >,
-//     // The contents of the files to match against, in the same order as `paths`.
-//     contents: Vec < String >,
-//     // Library file names, for the language of the pattern.
-//     lib_paths: Vec < String >,
-//     // Library file contents, in the same order as `lib_paths`.
-//     lib_contents: Vec < String >,
-//     // LLM API base
-//     llm_api_base: String,
-//     // LLM API bearer token
-//     llm_api_bearer_token: String,
-// )
-
 async function match(pattern, content) {
     return await matchPattern(
         pattern,
