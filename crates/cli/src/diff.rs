0387753e2d96cdf6639e34e419b4e2ff3fa440f8
Morgante Pell <morgantep@google.com>
2024-04-22 17:00:13 -0700

    [skip ci] yolo

--- crates/cli/src/diff.rs
+++ crates/cli/src/diff.rs
@@ -14,19 +14,11 @@ pub fn run_git_diff(path: &PathBuf) -> Result<String> {
     Ok(String::from_utf8(output.stdout)?)
 }
 
-pub fn extract_modified_ranges(diff_path: &PathBuf) -> Result<Vec<FileDiff>> {
-    let mut file = File::open(diff_path)?;
-    let mut diff = String::new();
-
-    file.read_to_string(&mut diff)?;
-    parse_modified_ranges(&diff)
-}
-
 pub(crate) fn extract_target_ranges(
-    arg: &Option<Option<PathBuf>>,
+    diff_arg: &Option<Option<String>>,
 ) -> Result<Option<Vec<FileRange>>> {
-    let raw_diff = if let Some(Some(diff_path)) = &arg {
-        extract_modified_ranges(diff_path)?
+    let raw_diff = if let Some(Some(diff_content)) = &diff_arg {
+        parse_modified_ranges(&diff_content)?
     } else if let Some(None) = &arg {
         let diff = run_git_diff(&std::env::current_dir()?)?;
         parse_modified_ranges(&diff)?
