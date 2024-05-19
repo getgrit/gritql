cf0a21fc33e573a088e3fcec10734cb4ea1a4594
Morgante Pell <morgantep@google.com>
2024-05-19 01:26:03 -0700

    [skip ci] yolo

--- crates/cli/src/resolver.rs
+++ crates/cli/src/resolver.rs
@@ -104,7 +104,6 @@ pub async fn get_grit_files_from_flags_or_cwd(
     flags: &GlobalFormatFlags,
 ) -> Result<PatternsDirectory> {
     if let Some(grit_dir) = &flags.grit_dir {
-        println!("Using grit dir: {}", grit_dir.display());
         from_known_grit_dir(grit_dir).await
     } else {
         let cwd = std::env::current_dir()?;
