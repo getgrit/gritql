4fdb7b980dc2aa770eae27b751c8cd9dd96f54ce
Morgante Pell <morgantep@google.com>
2024-05-23 13:54:34 -0700

    [skip ci] yolo

--- crates/language/src/notebooks.rs
+++ crates/language/src/notebooks.rs
@@ -1,9 +1,19 @@
+use grit_util::Ast;
+use grit_util::AstNode;
 use std::path::Path;
 
+use anyhow::bail;
+use anyhow::Result;
+use grit_util::traverse;
+use grit_util::Order;
+use grit_util::Range;
 use grit_util::{AnalysisLogs, Parser, SnippetTree};
+use marzano_util::cursor_wrapper::CursorWrapper;
 
-use crate::{json::Json, language::{MarzanoLanguage, MarzanoParser, Tree}};
-
+use crate::{
+    json::Json,
+    language::{MarzanoLanguage, MarzanoParser, Tree},
+};
 
 /// Custom Python parser, to include notebooks
 pub(crate) struct MarzanoNotebookParser(MarzanoParser);
@@ -28,30 +38,45 @@ impl grit_util::Parser for MarzanoNotebookParser {
             .and_then(Path::extension)
             .is_some_and(|ext| ext == "ipynb")
         {
-            let notebook_json = match serde_json::from_str::<Notebook>(body) {
-                Ok(notebook) => notebook,
-                Err(e) => {
-                    // logs.error("Failed to parse notebook JSON", e);
-                    return None;
-                }
-            };
+            // let json = Json::new(None);
+            // let mut parser = json.get_parser();
+            // let text = body.as_bytes();
+            // // parser.set_language(json.get_ts_language())?;
+            // let tree = parser.parse_file(body, None, logs, false)?;
+            // let root = tree.root_node().node;
+            // let mut cursor = root.walk();
 
-            let json_parser
+            // let root_doc = root.child_by_field_name("value")?;
+            // let properties = root_doc.child_by_field_name("properties")?;
 
-            let included_ranges = notebook_json
-                .cells
-                .iter()
-                .enumerate()
-                .map(|(i, cell)| {
-                    let start = cell.source.first().map(|s| s.len()).unwrap_or(0);
-                    let end = cell.source.last().map(|s| s.len()).unwrap_or(0);
-                    (start, end, i)
-                })
-                .collect::<Vec<_>>();
+            // // println!("Props: {:?}", properties);
 
-            println!("Notebook parsing not implemented");
+            // panic!("Notebook parsing not implemented");
 
-            panic!("Notebook parsing not implemented");
+            // let notebook_json = match serde_json::from_str::<Notebook>(body) {
+            //     Ok(notebook) => notebook,
+            //     Err(e) => {
+            //         // logs.error("Failed to parse notebook JSON", e);
+            //         return None;
+            //     }
+            // };
+
+            // let json_parser
+
+            // let included_ranges = notebook_json
+            //     .cells
+            //     .iter()
+            //     .enumerate()
+            //     .map(|(i, cell)| {
+            //         let start = cell.source.first().map(|s| s.len()).unwrap_or(0);
+            //         let end = cell.source.last().map(|s| s.len()).unwrap_or(0);
+            //         (start, end, i)
+            //     })
+            //     .collect::<Vec<_>>();
+
+            // println!("Notebook parsing not implemented");
+
+            // panic!("Notebook parsing not implemented");
             // let parent_node_kind = "style_element";
             // let ranges = get_vue_ranges(body, parent_node_kind, None).ok()?;
             // if ranges.is_empty() {
@@ -77,22 +102,4 @@ impl grit_util::Parser for MarzanoNotebookParser {
     ) -> SnippetTree<Tree> {
         self.0.parse_snippet(pre, source, post)
     }
-}
-
-pub(crate) fn get_cell_ranges(
-    file: &str,
-    parent_node_kind: &str,
-    name_array: Option<&[&str]>,
-) -> Result<Vec<Range>> {
-    let vue = Json::new(None);
-    let mut parser = Parser::new()?;
-    let text = file.as_bytes();
-    parser.set_language(vue.get_ts_language())?;
-    let tree = parser.parse(file, None)?.ok_or(anyhow!("missing tree"))?;
-    let cursor = tree.walk();
-    let mut ranges = Vec::new();
-    for n in traverse(CursorWrapper::new(cursor, file), Order::Pre) {
-        append_code_range(&n.node, text, &mut ranges, parent_node_kind, name_array)
-    }
-    Ok(ranges)
 }
