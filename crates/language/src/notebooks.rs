41865146f3099bb46b2c062c73a61c5c8fa03863
Morgante Pell <morgantep@google.com>
2024-05-23 16:58:22 -0700

    [skip ci] yolo

--- crates/language/src/notebooks.rs
+++ crates/language/src/notebooks.rs
@@ -45,22 +45,40 @@ fn is_code_cell_pair(node: &NodeWithSource, program_src: &[u8]) -> bool {
 /// Given a pair, extract the value
 fn get_pair_sources<'a>(
     node: &'a NodeWithSource,
-    program_src: &[u8],
-) -> Option<NodeWithSource<'a>> {
+    program_src: &str,
+) -> Option<Vec<tree_sitter::Range>> {
     if node.node.kind() != "pair" {
         return None;
     }
 
     if !node
         .child_by_field_name("key")
-        .and_then(|key| key.node.utf8_text(program_src).ok())
+        .and_then(|key| key.node.utf8_text(program_src.as_bytes()).ok())
         .map(|key| key == "\"source\"")
         .unwrap_or(false)
     {
         return None;
     }
 
-    node.child_by_field_name("value")
+    let Some(value) = node.child_by_field_name("value") else {
+        return None;
+    };
+
+    let mut ranges = Vec::new();
+
+    let children = value.node.walk();
+    let mut cursor = CursorWrapper::new(children, program_src);
+    for n in traverse(cursor, Order::Pre) {
+        if n.node.kind() == "string_content" {
+            ranges.push(n.node.range());
+        }
+    }
+
+    if ranges.is_empty() {
+        return None;
+    }
+
+    Some(ranges)
 }
 
 impl grit_util::Parser for MarzanoNotebookParser {
@@ -77,24 +95,13 @@ impl grit_util::Parser for MarzanoNotebookParser {
             .and_then(Path::extension)
             .is_some_and(|ext| ext == "ipynb")
         {
-            //             let gritql_pattern = r#"engine marzano(0.1)
-            // language json
-
-            // file($body) where {
-            //     $body <: contains bubble object($properties) where {
-            //         $properties <: some `"cell_type": "markdown"`,
-            //         $properties <: some `"source": [$source]`,
-            //         $source <: some bubble `"$partial_source"`
-            //     }
-            // }"#;
+            let mut all_ranges = Vec::new();
 
             let json = Json::new(None);
             let mut parser = json.get_parser();
-            // let text = body.as_bytes();
-            // parser.set_language(json.get_ts_language())?;
             let tree = parser.parse_file(body, None, logs, false)?;
             let root = tree.root_node().node;
-            let mut cursor = root.walk();
+            let cursor = root.walk();
 
             for n in traverse(CursorWrapper::new(cursor, body), Order::Pre) {
                 if n.node.kind() != "object" {
@@ -105,7 +112,7 @@ impl grit_util::Parser for MarzanoNotebookParser {
 
                 let mut is_code_cell = true;
 
-                // let source_cells = None;
+                let mut source_ranges = None;
 
                 cursor.goto_first_child(); // Enter the object
                 while cursor.goto_next_sibling() {
@@ -113,92 +120,37 @@ impl grit_util::Parser for MarzanoNotebookParser {
                     let node = cursor.node();
 
                     if is_code_cell_pair(&node, body.as_bytes()) {
-                        println!("Found code cell");
                         is_code_cell = true;
                     }
 
-                    // if node.node.kind() == "pair" {
-                    //     let key_node = node.child_by_field_name("key");
-                    //     if let Some(key) = key_node {
-                    //         let child = key.named_children().next();
-                    //         if let Some(child) = child {
-                    //             if child.node.utf8_text(body.as_bytes()).ok()
-                    //                 == Some(std::borrow::Cow::Borrowed("cell_type"))
-                    //             {
-                    //                 println!(
-                    //                     "We found the cell: {:?}",
-                    //                     child.node.utf8_text(body.as_bytes()).ok()?
-                    //                 );
-                    //             }
-                    //             println!("key: {:?}", child.node.utf8_text(body.as_bytes()).ok()?);
-                    //         }
-                    //     }
-
-                    // let value_node = node.child_by_field_name("value");
-                    // println!("key: {:?}", key_node);
-                    // if let (Some(key), Some(value)) = (key_node, value_node) {
-                    //     if key.utf8_text(text).ok()? == "\"code\"" {
-                    //         // Do something with the code value
-                    //         // For example, print it
-                    //         println!("Found code: {:?}", value.utf8_text(text).ok()?);
-                    //     }
-                    // }
-                    // }
+                    if let Some(ranges) = get_pair_sources(&node, body) {
+                        source_ranges = Some(ranges);
+                    }
                 }
-                cursor.goto_parent(); // Exit the object
 
-                // println!(
-                //     "node_kind: {}, node_text: {:?}",
-                //     n.node.kind(),
-                //     n.node.utf8_text(text)
-                // );
-                // append_code_range(&n.node, text, &mut ranges, parent_node_kind, name_array)
+                if is_code_cell {
+                    if let Some(source_cells) = source_ranges {
+                        all_ranges.extend(source_cells);
+                    }
+                }
+
+                cursor.goto_parent(); // Exit the object
             }
 
-            panic!("Notebook parsing not implemented");
+            println!("SOURCDE CODE!");
+            for range in &all_ranges {
+                println!(
+                    "{}",
+                    &body[range.start_byte() as usize..range.end_byte() as usize]
+                );
+            }
 
-            // // Document -> object
-            // cursor.goto_first_child();
-            // // Object -> values
-            // cursor.goto_first_child();
-
-            // while cursor.goto_next_sibling() {
-            //     let node = cursor.node();
-            //     if node.kind() != "pair" {
-            //         continue;
-            //     }
-            //     cursor.goto_first_child();
-            //     cursor.goto_first_child();
-            //     cursor.goto_first_child();
-            //     println!("node: {} - {:?}", node.kind(), node.utf8_text(text));
-            //     cursor.goto_parent();
-            //     cursor.goto_parent();
-            //     cursor.goto_parent();
-            // }
-
-            // let root_doc = root.child_by_field_name("value")?;
-            // let props = root_doc.children_by_field_name("properties", &mut cursor);
-
-            // for prop in props {
-            //     // println!("cell: {:?}", cell.utf8_text(text));
-            //     if let Some(cell_value) = prop.child_by_field_name("value") {
-            //         println!("cell_value: {:?}", cell_value.utf8_text(text));
-            //     }
-            // }
-
-            // panic!("Notebook parsing not implemented");
-            // let parent_node_kind = "style_element";
-            // let ranges = get_vue_ranges(body, parent_node_kind, None).ok()?;
-            // if ranges.is_empty() {
-            //     return None;
-            // }
-
-            // self.0.parser.set_included_ranges(&ranges).ok()?;
-            // self.0
-            //     .parser
-            //     .parse(body, None)
-            //     .ok()?
-            //     .map(|tree| Tree::new(tree, body))
+            self.0.parser.set_included_ranges(&all_ranges).ok()?;
+            self.0
+                .parser
+                .parse(body, None)
+                .ok()?
+                .map(|tree| Tree::new(tree, body))
         } else {
             self.0.parse_file(body, path, logs, new)
         }
