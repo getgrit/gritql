6686763a74cf2609afc6e1c69a45500693dddce1
Morgante Pell <morgantep@google.com>
2024-05-24 23:37:11 -0700

    [skip ci] yolo

--- crates/core/src/pattern_compiler/file_owner_compiler.rs
+++ crates/core/src/pattern_compiler/file_owner_compiler.rs
@@ -19,7 +19,6 @@ impl FileOwnerCompiler {
         let name = name.into();
         let new = !old_tree.is_fresh();
 
-        println!("New one, from {:?}", old_tree);
 
         // If we have an old tree, attach it here
         let new_map = if let Some(old_tree) = old_tree.original() {
