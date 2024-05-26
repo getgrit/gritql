9acb2d36b9dd79c67a0c73873b3c400c5d63221c
Morgante Pell <morgantep@google.com>
2024-05-26 14:23:48 -0700

    [skip ci] yolo

--- crates/core/src/test_notebooks.rs
+++ crates/core/src/test_notebooks.rs
@@ -287,4 +287,55 @@ mod tests {
             panic!("Expected a rewrite");
         }
     }
+
+    #[test]
+    fn test_weird_side_effects_orphans() {
+        let pattern_src = r#"
+        language python
+
+        or {
+            `from langchain.agents import $stuffs` as $anchor where {
+                $stuffs <: contains `load_tools` => .,
+                $anchor += `\nfrom my_thing insert tools`
+            }
+        }
+        "#;
+        let libs = BTreeMap::new();
+
+        let matching_src =
+            include_str!("../../../crates/cli_bin/fixtures/notebooks/langchain_open.ipynb");
+
+        let pattern = src_to_problem_libs(
+            pattern_src.to_string(),
+            &libs,
+            TargetLanguage::from_extension("ipynb").unwrap(),
+            None,
+            None,
+            None,
+            None,
+        )
+        .unwrap()
+        .problem;
+
+        // Basic match works
+        let test_files = vec![SyntheticFile::new(
+            "target.ipynb".to_owned(),
+            matching_src.to_owned(),
+            true,
+        )];
+        let results = run_on_test_files(&pattern, &test_files);
+        assert!(!results.iter().any(|r| r.is_error()));
+
+        let rewrite = results
+            .iter()
+            .find(|r| matches!(r, MatchResult::Rewrite(_)))
+            .unwrap();
+
+        if let MatchResult::Rewrite(rewrite) = rewrite {
+            assert!(!rewrite.rewritten.content.contains("\"gent_chain.run"));
+            assert_snapshot!(rewrite.rewritten.content);
+        } else {
+            panic!("Expected a rewrite");
+        }
+    }
 }
