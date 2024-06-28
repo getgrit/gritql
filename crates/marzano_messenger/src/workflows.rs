f9ffe6f72c3c529c7ed2e881a0236086060248b2
Morgante Pell <morgantep@google.com>
2024-06-28 16:09:21 -0700

    [skip ci] yolo

--- crates/marzano_messenger/src/workflows.rs
+++ crates/marzano_messenger/src/workflows.rs
@@ -1,4 +1,7 @@
+use marzano_core::api::MatchResult;
 use serde::{Deserialize, Serialize};
+
+use crate::emit::{Messager, VisibilityLevels};
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct PackagedWorkflowOutcome {
@@ -8,8 +11,17 @@ pub struct PackagedWorkflowOutcome {
 }
 
 /// Handle workflow-related messages
-pub trait WorkflowMessenger {
+pub trait WorkflowMessenger: Messager {
     fn save_metadata(&mut self, message: &SimpleWorkflowMessage) -> anyhow::Result<()>;
+
+    /// Emit a match result from a workflow, which has some additional metadata around workspace and paths
+    fn emit_from_workflow(&mut self, message: &WorkflowMatchResult) -> anyhow::Result<()> {
+        self.emit(
+            &message.result,
+            // This is meant to match what we do in the CLI server
+            &VisibilityLevels::Debug,
+        )
+    }
 }
 
 /// Simple workflow message representation, mainly intended for RPC
@@ -17,4 +29,11 @@ pub trait WorkflowMessenger {
 pub struct SimpleWorkflowMessage {
     pub kind: String,
     pub message: serde_json::Value,
+}
+
+/// Wrap match results to account for workflow logic and path normalization
+#[derive(Debug, Serialize, Deserialize)]
+pub struct WorkflowMatchResult {
+    pub result: MatchResult,
+    pub workspace_path: Option<String>,
 }
