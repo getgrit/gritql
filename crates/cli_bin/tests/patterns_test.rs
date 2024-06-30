00d411940815ce91fd200afe52d87ea2445cb490
Ashu999 <ashutosh.sharma068@gmail.com>
2024-06-30 22:56:14 +0530

    added test for watch mode case when all patterns are removed

--- crates/cli_bin/tests/patterns_test.rs
+++ crates/cli_bin/tests/patterns_test.rs
@@ -11,7 +11,7 @@ use anyhow::Result;
 use assert_cmd::cargo::CommandCargoExt;
 use insta::assert_snapshot;
 
-use crate::common::{get_fixture, get_test_cmd};
+use crate::common::{get_fixture, get_test_cmd, BIN_NAME};
 
 mod common;
 
@@ -321,7 +321,7 @@ fn tests_python_pattern_with_file_name() -> Result<()> {
 }
 
 #[test]
-fn watch_mode_of_patterns_test() -> Result<()> {
+fn patterns_test_watch_mode_case_patterns_changed() -> Result<()> {
     let (tx, rx) = mpsc::channel();
 
     let (temp_dir, temp_grit_dir) = get_fixture(".grit", false)?;
@@ -329,7 +329,7 @@ fn watch_mode_of_patterns_test() -> Result<()> {
     let temp_dir_path = temp_dir.path().to_owned();
 
     let _cmd_handle = thread::spawn(move || {
-        let mut cmd = Command::cargo_bin("marzano")
+        let mut cmd = Command::cargo_bin(BIN_NAME)
             .unwrap()
             .args(&["patterns", "test", "--watch"])
             .current_dir(&temp_dir_path)
@@ -361,6 +361,57 @@ fn watch_mode_of_patterns_test() -> Result<()> {
         "[Watch Mode] File modified: \".grit/grit.yaml\"",
         "[Watch Mode] Pattern(s) to test: [\"our_cargo_use_long_dependency\", \"cargo_use_long_dependency\", \"no_treesitter_in_grit_crates\", \"no_println_in_lsp\", \"no_println_in_core\"]",
         "Found 5 testable patterns.",
+    ];
+    for expected_line in expected_output {
+        assert!(
+            output.iter().any(|line| line.contains(expected_line)),
+            "Expected output not found: {}",
+            expected_line
+        );
+    }
+    Ok(())
+}
+
+#[test]
+fn patterns_test_watch_mode_case_no_pattern_to_test() -> Result<()> {
+    let (tx, rx) = mpsc::channel();
+
+    let (temp_dir, temp_grit_dir) = get_fixture(".grit", false)?;
+    let test_yaml_path = temp_grit_dir.join("grit.yaml");
+    let temp_dir_path = temp_dir.path().to_owned();
+
+    let _cmd_handle = thread::spawn(move || {
+        let mut cmd = Command::cargo_bin(BIN_NAME)
+            .unwrap()
+            .args(&["patterns", "test", "--watch"])
+            .current_dir(&temp_dir_path)
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()
+            .expect("Failed to start command");
+
+        let stdout = BufReader::new(cmd.stdout.take().unwrap());
+        let stderr = BufReader::new(cmd.stderr.take().unwrap());
+        for line in stdout.lines().chain(stderr.lines()) {
+            if let Ok(line) = line {
+                tx.send(line).unwrap();
+            }
+        }
+    });
+    thread::sleep(Duration::from_secs(1));
+
+    fs::write(&test_yaml_path, "").unwrap();
+    thread::sleep(Duration::from_secs(1));
+
+    let mut output = Vec::new();
+    while let Ok(line) = rx.try_recv() {
+        output.push(line);
+    }
+
+    let expected_output = vec![
+        "[Watch Mode] Enabled on path: .grit",
+        "[Watch Mode] File modified: \".grit/grit.yaml\"",
+        "[Watch Mode] Pattern(s) to test: []",
     ];
     for expected_line in expected_output {
         assert!(
