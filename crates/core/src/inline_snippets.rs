6531fca293892abe00c683cc8f716193af944c27
chintu-777 <160070339+chintu-777@users.noreply.github.com>
2024-07-12 15:10:24 +0530

    Update inline_snippets.rs

--- crates/core/src/inline_snippets.rs
+++ crates/core/src/inline_snippets.rs
@@ -350,28 +350,26 @@ fn delete_hanging_comma(
         .collect();
     
     // Flag to track if the last character was a comma
-    let mut last_was_comma = false;
+    let mut previous_char_was_comma = false;
     
     for (index, c) in chars {
         if Some(&index) != next_comma {
-            if c == ',' && last_was_comma {
+            next_comma = to_delete.next();
+            previous_char_was_comma = false;
+            continue;
+        }
+        if c == ',' {
+            if previous_char_was_comma {
                 continue;
             }
-            result.push(c);
-            last_was_comma = c == ',';
+            previous_char_was_comma = true;
         } else {
             // Keep track of ranges we need to expand into, since we deleted code in the range
             // This isn't perfect, but it's good enough for tracking cell boundaries
-            for (range, ..) in replacement_ranges.iter_mut().rev() {
-                if range.end >= index {
-                    range.end += 1;
-                    break;
-                }
+            previous_char_was_comma = false;
+
             }
-            ranges_updates = update_range_shifts(index + offset, &ranges_updates, &ranges);
-            next_comma = to_delete.next();
-            last_was_comma = false;
-        }
+        result.push(c);
     }
 
     for (r, u) in replacements.iter_mut().zip(ranges_updates) {
