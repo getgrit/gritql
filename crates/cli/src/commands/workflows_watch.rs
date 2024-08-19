d18340f6128268e71d45f4d9081fbcf11bad7e2d
Morgante Pell <morgantep@google.com>
2024-08-19 14:10:40 -0700

    run the tests

--- crates/cli/src/commands/workflows_watch.rs
+++ crates/cli/src/commands/workflows_watch.rs
@@ -1,4 +1,4 @@
-use anyhow::{bail, Result};
+use anyhow::Result;
 use clap::Args;
 
 use serde::Serialize;
@@ -18,26 +18,26 @@ pub async fn run_watch_workflow(
 ) -> Result<()> {
     #[cfg(feature = "remote_workflows")]
     {
-        let mut updater = Updater::from_current_bin().await?;
+        let mut updater = crate::updater::Updater::from_current_bin().await?;
 
         let auth = updater.get_valid_auth().await?;
 
-        let format = OutputFormat::from(parent);
-        let emitter = create_emitter(
+        let format = crate::flags::OutputFormat::from(_parent);
+        let emitter = crate::messenger_variant::create_emitter(
             &format,
             marzano_messenger::output_mode::OutputMode::default(),
             None,
             false,
             None,
             None,
-            VisibilityLevels::default(),
+            marzano_messenger::emit::VisibilityLevels::default(),
         )
         .await?;
-        grit_cloud_client::watch_workflow(&arg.workflow_id, &auth, emitter).await?;
+        grit_cloud_client::watch_workflow(&_arg.workflow_id, &auth, emitter).await?;
+
+        return Ok(());
     }
 
     #[cfg(not(feature = "remote_workflows"))]
-    bail!("Remote workflows are not supported on this platform");
-
-    Ok(())
+    anyhow::bail!("Remote workflows are not supported on this platform")
 }
