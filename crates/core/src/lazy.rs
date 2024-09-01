02d0b404e11d8833f67888ed43f8abd8dc057c96
Morgante Pell <morgantep@google.com>
2024-08-31 22:38:24 -0700

    register

--- crates/core/src/lazy.rs
+++ crates/core/src/lazy.rs
@@ -1,4 +1,11 @@
-use crate::marzano_resolved_pattern::MarzanoResolvedPattern;
+use grit_pattern_matcher::pattern::Matcher;
+use grit_pattern_matcher::pattern::Pattern;
+use grit_pattern_matcher::pattern::State;
+use grit_util::error::GritResult;
+use grit_util::AnalysisLogs;
+
+use crate::marzano_context::MarzanoContext;
+use crate::{marzano_resolved_pattern::MarzanoResolvedPattern, problem::MarzanoQueryContext};
 
 #[derive(Debug, Clone)]
 pub(crate) struct LazyTraversal<'a, 'b> {
@@ -8,5 +15,15 @@ pub(crate) struct LazyTraversal<'a, 'b> {
 impl<'a, 'b> LazyTraversal<'a, 'b> {
     pub(crate) fn new(root: &'b MarzanoResolvedPattern<'a>) -> Self {
         Self { root }
+    }
+
+    pub(crate) fn matches(
+        &self,
+        pattern: &'a Pattern<MarzanoQueryContext>,
+        context: &'a MarzanoContext<'a>,
+        state: &mut State<'a, MarzanoQueryContext>,
+        logs: &mut AnalysisLogs,
+    ) -> GritResult<bool> {
+        pattern.execute(self.root, state, context, logs)
     }
 }
