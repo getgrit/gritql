75d0f096f4e4e01b6a4ea7a26890d73d10d8baa6
Morgante Pell <morgantep@google.com>
2024-09-18 15:03:24 -0500

    feat: include build sha in doctor output

--- crates/cli/src/commands/doctor.rs
+++ crates/cli/src/commands/doctor.rs
@@ -26,6 +26,13 @@ pub(crate) async fn run_doctor(_arg: DoctorArgs) -> Result<()> {
     info!("  OS: {}", target_os.yellow());
     info!("  Architecture: {}", target_arch.yellow());
 
+    let build_sha = option_env!("GRIT_BUILD_SHA");
+    if let Some(build_sha) = build_sha {
+        info!("  Build SHA: {}", build_sha.yellow());
+    } else {
+        info!("  Build SHA: unknown");
+    }
+
     let mut updater = Updater::from_current_bin().await?;
 
     info!("{}", "Configuration".bold());
