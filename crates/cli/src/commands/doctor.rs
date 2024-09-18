1efdb09812f4c87b12305de28a01b17f673aee03
Morgante Pell <morgantep@google.com>
2024-09-18 15:13:50 -0500

    run the tests

--- crates/cli/src/commands/doctor.rs
+++ crates/cli/src/commands/doctor.rs
@@ -26,8 +26,9 @@ pub(crate) async fn run_doctor(_arg: DoctorArgs) -> Result<()> {
     info!("  OS: {}", target_os.yellow());
     info!("  Architecture: {}", target_arch.yellow());
 
-    let build_sha = option_env!("GITHUB_SHA");
-    if let Some(build_sha) = build_sha {
+    let github_sha = option_env!("GITHUB_SHA");
+    let grit_sha = option_env!("GRIT_BUILD_SHA");
+    if let Some(build_sha) = grit_sha.or(github_sha) {
         info!("  Build SHA: {}", build_sha.yellow());
     } else {
         info!("  Build SHA: unknown");
