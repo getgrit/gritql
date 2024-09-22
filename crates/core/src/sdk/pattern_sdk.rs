4eea17eaa69ca911de5a694d2d553a41c04b3d89
Morgante Pell <morgantep@google.com>
2024-09-22 14:40:30 -0500

    oh wow ok

--- crates/core/src/sdk/pattern_sdk.rs
+++ crates/core/src/sdk/pattern_sdk.rs
@@ -1,3 +1,7 @@
-pub enum PatternBuilder {
-    Contains { child: Box<PatternBuilder> },
+/// UncompiledPattern is used to build up complex patterns *before* building them
+/// Late compilation allows us to reuse the same pattern across languages (where a snippet will ultimately be parsed differently
+/// It also allows the pattern to be used as a root pattern, or dynamically inside a function callback
+pub enum UncompiledPattern {
+    Contains { contains: Box<UncompiledPattern> },
+    Snippet { text: String },
 }
