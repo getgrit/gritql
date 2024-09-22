6e22e0e0de3f9c0ea46ca3273f1cf3a1d0991cbf
Morgante Pell <morgantep@google.com>
2024-09-22 01:37:33 -0500

    [skip ci] yolo

--- crates/core/src/sdk/test_js.rs
+++ crates/core/src/sdk/test_js.rs
@@ -1,8 +1,10 @@
 #[cfg(test)]
 mod tests {
-    use grit_pattern_matcher::pattern::{Contains, FilePattern, Pattern};
+    use grit_pattern_matcher::pattern::{Contains, DynamicPattern, FilePattern, Pattern, Rewrite};
+    use insta::assert_snapshot;
 
     use crate::{
+        api::MatchResult,
         sdk::language_sdk::LanguageSdk,
         test_utils::{run_on_test_files, SyntheticFile},
     };
@@ -102,5 +104,51 @@ mod tests {
         );
         println!("{:?}", results);
         assert_eq!(results.len(), 3);
+    }
+
+    #[test]
+    fn test_basic_snippet_with_rewrite() {
+        let mut sdk = LanguageSdk::default();
+
+        let snippet = sdk.snippet("console.log").unwrap();
+
+        let rewrite = Rewrite::new_pattern(
+            snippet,
+            DynamicPattern::from_str_constant("replaced").unwrap(),
+            None,
+        );
+
+        let results = run_on_test_files(
+            &sdk.build(rewrite).unwrap(),
+            &[
+                SyntheticFile::new(
+                    "test.js".to_owned(),
+                    "function() {
+                    console.log('hello world');
+                    console.log('message two');
+                }"
+                    .to_owned(),
+                    true,
+                ),
+                SyntheticFile::new(
+                    "bad.js".to_owned(),
+                    "function() {
+                    // no match here
+                }"
+                    .to_owned(),
+                    true,
+                ),
+            ],
+        );
+        println!("{:?}", results);
+        assert_eq!(results.len(), 3);
+        let mut found_rewrite = false;
+        for result in results {
+            if let MatchResult::Rewrite(rewrite) = result {
+                found_rewrite = true;
+                assert_snapshot!(rewrite.rewritten.content.as_deref().unwrap());
+            }
+        }
+        assert!(found_rewrite);
     }
 }
