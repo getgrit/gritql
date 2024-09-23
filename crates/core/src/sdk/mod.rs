0664cde02f7e088f13e043bffbbf0dbc83b212b5
Morgante Pell <morgantep@google.com>
2024-09-23 15:48:06 -0500

    [skip ci] yolo

--- crates/core/src/sdk/mod.rs
+++ crates/core/src/sdk/mod.rs
@@ -3,6 +3,9 @@ mod language_sdk;
 mod pattern_sdk;
 mod test_js;
 
+#[cfg(feature = "napi")]
+mod binding;
+
 pub(crate) use compiler::StatelessCompilerContext;
 pub use language_sdk::LanguageSdk;
 pub use pattern_sdk::UncompiledPatternBuilder;
