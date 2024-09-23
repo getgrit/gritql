67db42861d3aa44010001a675666f56e98d416a0
Morgante Pell <morgantep@google.com>
2024-09-23 15:57:25 -0500

    [skip ci] yolo

--- crates/core/src/sdk/binding.rs
+++ crates/core/src/sdk/binding.rs
@@ -4,6 +4,7 @@ use grit_pattern_matcher::pattern::PatternOrResolved;
 use grit_pattern_matcher::pattern::ResolvedPattern;
 use grit_pattern_matcher::pattern::State;
 
+use grit_pattern_matcher::pattern::Variable;
 use napi::{Env, Result};
 
 use crate::marzano_context::MarzanoContext;
@@ -79,6 +80,40 @@ impl ResultBinding {
         Ok(range.map(|e| e.into()))
     }
 
+    /// Retrieves the absolute file name of the file containing the current binding.
+    #[napi]
+    pub fn filename(&self, _env: Env) -> Result<String> {
+        let var = Variable::file_name();
+        let resolved = var
+            .get_pattern_or_resolved(self.state)
+            .map_err(|e| napi::Error::from_reason(format!("{:?}", e)))?;
+        let Some(candidate) = resolved else {
+            return Err(napi::Error::from_reason("No resolved pattern found"));
+        };
+        let PatternOrResolved::Resolved(resolved) = candidate else {
+            return Err(napi::Error::from_reason("No resolved pattern found"));
+        };
+        let stringified_result = resolved
+            .text(&self.state.files, self.context.language())
+            .map_err(|e| napi::Error::from_reason(format!("{:?}", e)))?;
+        let string = stringified_result.to_string();
+        Ok(string)
+    }
+
+    /// Inserts the provided text after the binding.
+    #[napi]
+    pub fn insert_after(&mut self, _env: Env, text: String) -> Result<()> {
+        let left = PatternOrResolved::Resolved::<MarzanoQueryContext>(self.inner);
+        let replacement = ResolvedPattern::from_string(text);
+
+        insert_effect(&left, replacement, self.state, self.context)
+            .map_err(|e| napi::Error::from_reason(format!("{:?}", e)))?;
+
+        Ok(())
+    }
+}
+
+
     /// Retrieve a variable's text value.
     // #[napi]
     // pub fn var(&self, _env: Env, name: String) -> Result<Option<String>> {
@@ -101,36 +136,3 @@ impl ResultBinding {
     //     let string = stringified_result.to_string();
     //     Ok(Some(string))
     // }
-
-    /// Retrieves the absolute file name of the file containing the current binding.
-    // #[napi]
-    // pub fn filename(&self, _env: Env) -> Result<String> {
-    //     let var = Variable::file_name();
-    //     let resolved = var
-    //         .get_pattern_or_resolved(self.state)
-    //         .map_err(|e| napi::Error::from_reason(format!("{:?}", e)))?;
-    //     let Some(candidate) = resolved else {
-    //         return Err(napi::Error::from_reason("No resolved pattern found"));
-    //     };
-    //     let PatternOrResolved::Resolved(resolved) = candidate else {
-    //         return Err(napi::Error::from_reason("No resolved pattern found"));
-    //     };
-    //     let stringified_result = resolved
-    //         .text(&self.state.files, self.context.language())
-    //         .map_err(|e| napi::Error::from_reason(format!("{:?}", e)))?;
-    //     let string = stringified_result.to_string();
-    //     Ok(string)
-    // }
-
-    /// Inserts the provided text after the binding.
-    #[napi]
-    pub fn insert_after(&mut self, _env: Env, text: String) -> Result<()> {
-        let left = PatternOrResolved::Resolved::<MarzanoQueryContext>(self.inner);
-        let replacement = ResolvedPattern::from_string(text);
-
-        insert_effect(&left, replacement, self.state, self.context)
-            .map_err(|e| napi::Error::from_reason(format!("{:?}", e)))?;
-
-        Ok(())
-    }
-}
