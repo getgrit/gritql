567927bd0df65df42ed486e9cd3d514429e9fa8a
Morgante Pell <me@morgante.net>
2024-10-10 01:35:21 -0500

    [skip ci] yolo

--- crates/cli_bin/tests/doctor.rs
+++ crates/cli_bin/tests/doctor.rs
@@ -20,3 +20,36 @@ fn runs_doctor_in_fixture() -> Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn test_runs_from_read_only_dir() -> Result<()> {
+    let bin_path = cargo_bin(env!("CARGO_PKG_NAME"));
+    let temp_dir = tempfile::tempdir()?;
+
+    let install_dir = temp_dir.path().join("layer1").join("layer2").join("layer3");
+    fs::create_dir_all(&install_dir)?;
+
+    let dest_path = install_dir.join("install");
+
+    fs::copy(bin_path, &dest_path)?;
+
+    // Make the temp dir read-only
+    let mut perms = fs::metadata(&temp_dir)?.permissions();
+    perms.set_readonly(true);
+    fs::set_permissions(&temp_dir, perms)?;
+
+    let mut cmd = Command::new(dest_path);
+    cmd.arg("doctor");
+
+    let stderr = cmd.output()?.stderr;
+    let stderr_str = String::from_utf8_lossy(&stderr);
+    println!("stderr: {}", stderr_str);
+
+    let stdout = cmd.output()?.stdout;
+    let stdout_str = String::from_utf8_lossy(&stdout);
+    println!("stdout: {}", stdout_str);
+
+    panic!("test failed");
+
+    Ok(())
+}
