855a1cb7e8073159224a978052fa30c5dd853696
Arian Rezazadeh <notarian.rez@gmail.com>
2025-01-02 12:06:16 +0330

    refactor: move format for loop body to different function

--- crates/cli/src/commands/format.rs
+++ crates/cli/src/commands/format.rs
@@ -5,6 +5,7 @@ use crate::{
 use anyhow::{ensure, Context, Result};
 use biome_grit_formatter::context::GritFormatOptions;
 use clap::Args;
+use marzano_gritmodule::config::ResolvedGritDefinition;
 use serde::Serialize;
 
 #[derive(Args, Debug, Serialize)]
@@ -16,43 +17,52 @@ pub struct FormatArgs {
 
 pub async fn run_format(arg: &FormatArgs) -> Result<()> {
     let (resolved, _) = resolve_from_cwd(&Source::Local).await?;
-    // TODO: make this run in parallel
+    // TODO: make this run in parallel, if needed
     for definition in resolved {
-        let old_body = &definition.body;
-        let parsed = biome_grit_parser::parse_grit(old_body);
-        let path = &definition.config.path;
-        ensure!(
-            parsed.diagnostics().is_empty(),
-            "couldn't parse '{path}': {}",
-            parsed
-                .diagnostics()
-                .iter()
-                .map(|diag| diag.message.to_string())
-                .collect::<Vec<_>>()
-                .join("\n")
-        );
-
-        let options = GritFormatOptions::default();
-        let doc = biome_grit_formatter::format_node(options, &parsed.syntax())
-            .with_context(|| format!("couldn't format '{path}'"))?;
-        let doc_print = doc.print()?;
-        let new_body = doc_print.as_code();
-
-        if arg.write {
-            // TODO: i think there is already a rewriting feature that `apply` subcommand uses
-            // that i think can be re used here, look into it or at least
-            // use definition.config.range instead of replacing
-            let raw_data = definition.config.raw.as_ref().unwrap();
-            tokio::fs::write(path, raw_data.content.replace(old_body, new_body))
-                .await
-                .with_context(|| format!("could not write to file at '{path}'"))?;
-        } else {
-            println!(
-                "{}:\n{}",
-                definition.config.path,
-                format_diff(&definition.body, new_body)
-            );
+        if let Err(error) = format_resolv(&definition, &arg).await {
+            eprintln!("couldn't format '{}': {error:?}", definition.config.path)
         }
+    }
+    Ok(())
+}
+
+async fn format_resolv(definition: &ResolvedGritDefinition, arg: &FormatArgs) -> Result<()> {
+    let old_body = &definition.body;
+    let parsed = biome_grit_parser::parse_grit(old_body);
+    ensure!(
+        parsed.diagnostics().is_empty(),
+        "biome couldn't parse: {}",
+        parsed
+            .diagnostics()
+            .iter()
+            .map(|diag| diag.message.to_string())
+            .collect::<Vec<_>>()
+            .join("\n")
+    );
+
+    let options = GritFormatOptions::default();
+    let doc = biome_grit_formatter::format_node(options, &parsed.syntax())
+        .with_context(|| "biome couldn't format")?;
+    let doc_print = doc.print()?;
+    let new_body = doc_print.as_code();
+
+    if arg.write {
+        // TODO: i think there is already a rewriting feature that `apply` subcommand uses
+        // that i think can be re used here, look into it or at least
+        // use definition.config.range instead of replacing
+        let raw_data = definition.config.raw.as_ref().unwrap();
+        tokio::fs::write(
+            &definition.config.path,
+            raw_data.content.replace(old_body, new_body),
+        )
+        .await
+        .with_context(|| "could not write to file")?;
+    } else {
+        println!(
+            "{}:\n{}",
+            definition.config.path,
+            format_diff(&definition.body, new_body)
+        );
     }
     Ok(())
 }
