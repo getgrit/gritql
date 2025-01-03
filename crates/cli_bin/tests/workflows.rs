721eaa92264b0a1dbe3c2795fc6b1032339eb49b
Morgante Pell <me@morgante.net>
2025-01-03 00:21:12 -0600

    chore: allow unauthenticated workflow exection

--- crates/cli_bin/tests/workflows.rs
+++ crates/cli_bin/tests/workflows.rs
@@ -132,3 +132,27 @@ fn applies_user_workflows() -> Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn run_workflow_args() -> Result<()> {
+    let (_temp_dir, temp_fixtures_root) = get_fixture("grit_modules", true)?;
+
+    let mut cmd = get_test_cmd()?;
+    cmd.arg("apply")
+        .arg("https://storage.googleapis.com/grit-workflows-dev-workflow_definitions/test/hello.js")
+        .current_dir(temp_fixtures_root);
+
+    let output = cmd.output()?;
+    println!("stdout: {:?}", String::from_utf8(output.stdout.clone())?);
+    println!("stderr: {:?}", String::from_utf8(output.stderr.clone())?);
+
+    assert!(
+        output.status.success(),
+        "Command didn't finish successfully"
+    );
+
+    let stdout = String::from_utf8(output.stdout)?;
+    assert!(stdout.contains("Running hello workflow"),);
+
+    Ok(())
+}
