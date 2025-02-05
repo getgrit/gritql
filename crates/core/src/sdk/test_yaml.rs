86fec699e4b29c1e459d93c741b7e06b80e50059
Morgante Pell <me@morgante.net>
2025-02-05 14:41:08 -0600

    [skip ci] yolo

--- crates/core/src/sdk/test_yaml.rs
+++ crates/core/src/sdk/test_yaml.rs
@@ -1,5 +1,7 @@
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
+
     use grit_pattern_matcher::pattern::{Contains, DynamicPattern, FilePattern, Pattern, Rewrite};
     use insta::assert_snapshot;
     use itertools::Itertools;
@@ -45,6 +47,64 @@ mod tests {
                 SyntheticFile::new("bad.yaml".to_owned(), "bar: baz\n".to_owned(), true),
             ],
         );
+
+        assert!(
+            results
+                .iter()
+                .filter(|r| matches!(r, MatchResult::Match(_)))
+                .exactly_one()
+                .is_ok(),
+            "should have exactly one match"
+        );
+    }
+
+    #[test]
+    fn test_yaml_patterns() {
+        let mut sdk =
+            LanguageSdk::from_language(TargetLanguage::from_string("yaml", None).unwrap());
+
+        let yaml_content = r#"version: 0.0.1
+patterns:
+  - name: github.com/getgrit/stdlib#*
+  - name: other_pattern
+    level: error
+    body: |
+      language toml
+
+      other_pattern() where $filename <: includes "test.yaml"
+  - name: target_pattern
+    level: error
+    body: |
+      language foolhardy
+  - name: our_cargo_use_long_dependency
+    level: error
+    body: |
+      language toml
+
+      cargo_use_long_dependency() where $filename <: not includes or {
+        "language-submodules",
+        "language-metavariables"
+      }"#;
+
+        let our_name = sdk
+            .compiler()
+            .node("block_mapping_pair", HashMap::from([("key", Pattern::Top)]))
+            .unwrap();
+
+        let file = Pattern::File(Box::new(FilePattern::new(
+            Pattern::Top,
+            Contains::new_pattern(our_name, None),
+        )));
+
+        let results = run_on_test_files(
+            &sdk.build(file).unwrap(),
+            &[
+                SyntheticFile::new("test.yaml".to_owned(), yaml_content.to_owned(), true),
+                SyntheticFile::new("bad.yaml".to_owned(), "funky\n".to_owned(), true),
+            ],
+        );
+
+        println!("{:?}", results);
 
         assert!(
             results
