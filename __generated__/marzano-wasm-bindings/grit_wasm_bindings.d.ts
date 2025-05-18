13b0c8621317dc58cefd473944fae240757d47b8
Morgante Pell <me@morgante.net>
2025-05-18 15:03:29 -0700

    add wasm bindings

---
+++ __generated__/marzano-wasm-bindings/grit_wasm_bindings.d.ts
@@ -0,0 +1,81 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @returns {Promise<void>}
+*/
+export function initializeTreeSitter(): Promise<void>;
+/**
+* @param {string} pattern
+* @param {(string)[]} paths
+* @param {(string)[]} contents
+* @param {(string)[]} lib_paths
+* @param {(string)[]} lib_contents
+* @returns {Promise<any>}
+*/
+export function parseInputFiles(pattern: string, paths: (string)[], contents: (string)[], lib_paths: (string)[], lib_contents: (string)[]): Promise<any>;
+/**
+* @param {UncompiledPatternBuilder} pattern
+* @param {(string)[]} paths
+* @param {(string)[]} contents
+* @param {(string)[]} lib_paths
+* @param {(string)[]} lib_contents
+* @param {string} llm_api_base
+* @param {string} llm_api_bearer_token
+* @returns {Promise<any>}
+*/
+export function runPattern(pattern: UncompiledPatternBuilder, paths: (string)[], contents: (string)[], lib_paths: (string)[], lib_contents: (string)[], llm_api_base: string, llm_api_bearer_token: string): Promise<any>;
+/**
+* @param {string} pattern
+* @param {(string)[]} paths
+* @param {(string)[]} contents
+* @param {(string)[]} lib_paths
+* @param {(string)[]} lib_contents
+* @param {string} llm_api_base
+* @param {string} llm_api_bearer_token
+* @returns {Promise<any>}
+*/
+export function matchPattern(pattern: string, paths: (string)[], contents: (string)[], lib_paths: (string)[], lib_contents: (string)[], llm_api_base: string, llm_api_bearer_token: string): Promise<any>;
+/**
+*/
+export class UncompiledPatternBuilder {
+  free(): void;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly initializeTreeSitter: () => number;
+  readonly parseInputFiles: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number) => number;
+  readonly runPattern: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number, k: number, l: number, m: number) => number;
+  readonly matchPattern: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number, k: number, l: number, m: number, n: number) => number;
+  readonly __wbg_uncompiledpatternbuilder_free: (a: number) => void;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_export_2: WebAssembly.Table;
+  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h514cdf5dd6f00f9c: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke2_mut__h3ee746afe2fcb399: (a: number, b: number, c: number, d: number) => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ __generated__/marzano-wasm-bindings/marzano_wasm_bindings_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function initializeTreeSitter(): number;
+export function parseInputFiles(a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number): number;
+export function matchPattern(a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number, k: number, l: number, m: number, n: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h6088d10f3dade2dc(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__h3abfb7d3f6993b4a(a: number, b: number, c: number, d: number): void;
