40d78e1be9c4c936d6143bb440783d943685f113
Morgante Pell <me@morgante.net>
2025-05-18 14:43:03 -0700

    no gate

--- docs/src/components/gate.tsx
+++ docs/src/components/gate.tsx
@@ -1,16 +1,9 @@
 import { WithChildren } from '@/custom-types/shared';
-import { useFeatureFlag } from '@/components/analytics/flags';
-import { FeatureFlag } from '@getgrit/universal';
 
 type GateProps = WithChildren<{
-  flag: FeatureFlag;
+  flag: string;
 }>;
 
 export const GateTag = (props: GateProps) => {
-  const flag = useFeatureFlag(props.flag);
-  if (!flag) {
-    return null;
-  }
-
-  return <>{props.children}</>;
+  return null;
 };
