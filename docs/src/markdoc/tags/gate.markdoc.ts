5cf923f8b7cef1ccff1cf2d475de390c7f029bfa
Morgante Pell <me@morgante.net>
2025-05-18 14:47:53 -0700

    gate safety

--- docs/src/markdoc/tags/gate.markdoc.ts
+++ docs/src/markdoc/tags/gate.markdoc.ts
@@ -1,12 +1,10 @@
 import { GateTag } from '@/components/gate';
-import { FEATURE_FLAGS } from '@getgrit/universal';
 
 export const gate = {
   attributes: {
     flag: {
       type: String,
       required: true,
-      matches: Object.keys(FEATURE_FLAGS),
     },
   },
   render: GateTag,
